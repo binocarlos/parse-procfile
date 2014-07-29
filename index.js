349b5f3486aa8850a8ddcc4b7359f9318886bafe
Kai Davenport <kaiyadavenport@gmail.com>
2014-07-29 11:40:08 -0700

    tests passing

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 module.exports = function(content){
-	
+	var lines = content.split(/\r?\n/).filter(function(line, i){
+		if(line.match(/\w/)){
+			if(!line.match(/^\s*\w+:/)){
+				throw new Error('line ' + (i+1) + ' parse error: ' + line)
+			}
+		}
+		return line.match(/\w/)
+	})
+
+	var ret = {}
+
+	lines.forEach(function(line){
+		var parts = line.split(':')
+		var key = parts.shift().replace(/\s*$/, '')
+		ret[key] = parts.join(':').replace(/^\s*/, '')
+	})
+
+	return ret
 }
--- test.js
+++ test.js
@@ -3,11 +3,18 @@ var fs = require('fs')
 var parse = require('./')
 var path = require('path')
 
+tape('throw for a bad Procfile', function(t){
+	t.throws(function(){
+		var content = fs.readFileSync(path.join(__dirname, 'test/BadProcfile'), 'utf8')
+		var obj = parse(content)
+	}, null, 'parse bad procfile')
+	t.end()
+})
+
 tape('parse the Procfile', function(t){
 	var content = fs.readFileSync(path.join(__dirname, 'test/Procfile'), 'utf8')
 	var obj = parse(content)
 	t.equal(obj.web, 'node myserver --port 80', 'web')
 	t.equal(obj.db, 'mysql --port 3306', 'db')
-	console.dir(obj)
 	t.end()
 })
